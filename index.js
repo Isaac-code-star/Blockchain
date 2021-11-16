0b2f8f04f714026044ac72b24805142217c9c580
onovuakpo isaac <onovuakpoisaac@gmail.com>
2021-11-16 20:33:39 +0100

    final

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Copy the user wallet address
 let copyText = document.querySelector(".copy-address")
-copyText.querySelector("button").addEventListener("click", ()=>{
+copyText.querySelector(".button").addEventListener("click", function(){
     let input = copyText.querySelector("input.text");
     input.select();
     document.execCommand("copy")
